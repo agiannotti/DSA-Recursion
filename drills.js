2c1c4beab5a36e56a3ab79f178eb4569590af399
tonyg <aj.gianno@gmail.com>
2021-02-08 11:50:45 -0600

    drill 3

--- drills.js
+++ drills.js
@@ -2,17 +2,39 @@ function countSheep(num) {
   if (num === 0) {
     return;
   }
-  countSheep(num - 1);
+  return countSheep(num - 1);
 }
 
-countSheep(3);
+console.log(countSheep(3));
 
 function powerCalc(num, exp) {
   if (exp <= 0) {
     return 'exponent should be >= 0';
   }
   const powerVal = Math.pow(num, exp);
-  console.log('powerVal', powerVal);
+  return 'powerVal', powerVal;
 }
 
-powerCalc(10, 2);
+console.log(powerCalc(10, 2));
+
+function powerCalculator(base, exp) {
+  //Base case
+  if (exp === 0) {
+    return 1;
+  }
+  //General case
+  {
+    return base * powerCalculator(base, exp - 1);
+  }
+}
+
+console.log(powerCalculator(4, 2));
+
+function revStr(str) {
+  if (str === '') {
+    return '';
+  }
+  return revStr(str.substr(1)) + str.charAt(0);
+}
+
+console.log(revStr('goooooooolligans'));
